cad3bd3cde669255e5edefc701792b537fd42027
MarvinBlanca-LIGPH <76429291+MarvinBlanca-LIGPH@users.noreply.github.com>
2021-01-22 15:14:17 +0800

    01-21-2021 - Firebase Cloud Messaging (changes)

--- app/src/main/java/com/example/fcm/MainViewModel.kt
+++ app/src/main/java/com/example/fcm/MainViewModel.kt
@@ -1,11 +1,6 @@
 package com.example.fcm
 
-import android.widget.Toast
 import androidx.lifecycle.*
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.messaging.ktx.messaging
 
 class MainViewModel(
     private val activity: MainActivity
@@ -23,24 +18,16 @@ class MainViewModel(
         _kotlinText.value = activity.resources.getString(R.string.topic_kotlin)
         _androidText.value = activity.resources.getString(R.string.topic_android)
 
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                println("Fetching FCM registration token failed ${task.exception}")
-                return@OnCompleteListener
-            }
-
-            val token = task.result
-            println("Token: $token")
-        })
+        FirebaseSingleton.getToken()
     }
 
     fun subscribeKotlin() {
         if (!isSubKotlin) {
-            subscribeTo("kotlin")
+            FirebaseSingleton.subscribe("kotlin")
             _kotlinText.value = activity.resources.getString(R.string.unsubscribe_kotlin)
             isSubKotlin = true
         } else {
-            unsubscribeTo("kotlin")
+            FirebaseSingleton.unsubscribe("kotlin")
             _kotlinText.value = activity.resources.getString(R.string.topic_kotlin)
             isSubKotlin = false
         }
@@ -48,37 +35,13 @@ class MainViewModel(
 
     fun subscribeAndroid() {
         if (!isSubAndroid) {
-            subscribeTo("android")
+            FirebaseSingleton.subscribe("android")
             _androidText.value = activity.resources.getString(R.string.unsubscribe_android)
             isSubAndroid = true
         } else {
-            unsubscribeTo("android")
+            FirebaseSingleton.unsubscribe("android")
             _androidText.value = activity.resources.getString(R.string.topic_android)
             isSubAndroid = false
         }
-    }
-
-    private fun subscribeTo(topic: String) {
-        Firebase.messaging.subscribeToTopic(topic)
-            .addOnCompleteListener { task ->
-                var msg = activity.resources.getString(R.string.msg_subscribed)
-
-                if (!task.isSuccessful) {
-                    msg = activity.resources.getString(R.string.msg_subscribe_failed)
-                }
-                Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show()
-            }
-    }
-
-    private fun unsubscribeTo(topic: String) {
-        Firebase.messaging.unsubscribeFromTopic(topic)
-            .addOnCompleteListener { task ->
-                var msg = activity.resources.getString(R.string.msg_unsubscribed)
-
-                if (!task.isSuccessful) {
-                    msg = activity.resources.getString(R.string.msg_unsubscribe_failed)
-                }
-                Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show()
-            }
     }
 }
